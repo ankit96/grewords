cad1c940ade1a7594f5d37667ff347f05000ec6e
Ankit <ankit.sagwekar15@gmail.com>
2016-11-05 19:46:39 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+__author__ = 'ankit'
+import os
+from random import shuffle
+
+
+
+with open ("dictionary.txt")as f:
+	lines=f.readlines()
+
+#print str(lines)
+dic=[]
+for word in lines:
+	flag=0
+	if "[" in word:
+		start=word.index('[')
+	else:
+		flag=1
+	if "]" in word:
+		end=word.index(']')
+	
+	s=word[:start-1]
+	e=word[start+1:end]
+	#print s
+	#print e
+	a=s.split("=")
+	if flag==0:
+		a.append(e)
+	dic.append(a)
+shuffle(dic)
+for a in dic:
+	if len(a[0])<=2:
+		dic.remove(a)
+	else:	
+		print str(a[0])+" ==> " +str(a[1])+" ==> " +str(a[2])
+
+print len(dic)
+
+
